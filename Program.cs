c674e32de7e6383e93989aaba3189782531b9f81
Rafael <faelmota7@gmail.com>
2022-09-16 22:43:48 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("----Vivendo de dividendos----");
+Console.ResetColor();
+
+Console.Write("Gastos mensais (em R$): ");
+double gastos = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Rendimentos mensais (%): ");
+double rendimentos = Convert.ToDouble(Console.ReadLine());
+
+
+
+
